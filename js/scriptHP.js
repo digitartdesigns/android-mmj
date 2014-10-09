fce8c212e128da1e4cea1d209e3af833d270aa8f
digitartdesigns <florin@digitartdesigns.com>
2014-10-09 12:57:39 -0700

    index

--- js/scriptHP.js
+++ js/scriptHP.js
@@ -1,4 +1,13 @@
 $(document).ready(function() {
-var url = "https://goldstarmmj.com/?app";    
-$(location).attr('href',url);
+/*var url = "https://goldstarmmj.com/?app";    
+$(location).attr('href',url);*/
+$(" a").click(function (e) {
+			var inapp = $(this).attr("target");
+			var inappform = $(this).attr("target");
+			if(inapp=="_blank"){
+			e.preventDefault();
+			var targetURL = encodeURI($(this).attr("href"));
+			window.open(targetURL, "_blank",'location=yes');
+			 }
+		   });
 });
